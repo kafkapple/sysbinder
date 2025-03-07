833f65f92b9a6600b1e816118d14abfced294df8
kafkapple <biasdrive@gmail.com>
2025-03-07 19:21:28 +0900

    refactor: data down func -> data.py

--- data.py
+++ data.py
@@ -298,3 +298,29 @@ class EmotionTextDataset(Dataset):
             'top_words': top_words,
             'emotion_specific_words': emotion_words
         }
+
+import gdown
+import tarfile
+
+def download_clevr(type = 'easy'):
+    #https://drive.google.com/drive/folders/1FKEjZnKfu9KnSGfnr8oGVUBSPqnptzJc
+    if type == 'easy':
+        # 파일 ID 및 다운로드 URL 설정
+        file_id = '18zzqG50xjBI2cqOiG0MSQ4_C5IcEbdZV' #clever-easy
+        url = f'https://drive.google.com/uc?id={file_id}'
+        output = 'clevr-easy.tar.gz'
+    else:
+        print('Invalid type')
+        return
+    # 파일 다운로드
+    gdown.download(url, output, quiet=False)
+
+    # 압축 해제 경로 설정
+    extract_path = './data'
+
+    # tar.gz 파일 압축 해제
+    with tarfile.open(output, 'r:gz') as tar:
+        tar.extractall(path=extract_path)
+
+if __name__ == '__main__':
+    download_clevr(type='easy')
